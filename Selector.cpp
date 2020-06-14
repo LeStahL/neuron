5382ea976f2b117954b1996df5e452b75767b8b6
Alexander Kraus <nr4@z10.info>
2020-06-14 15:23:19 +0200

    removed python madness and fixed nfo instead

--- Selector.cpp
+++ Selector.cpp
@@ -3,11 +3,6 @@
 #include <QDebug>
 #include <QFile>
 
-#pragma push_macro("slots")
-#undef slots
-#include "Python.h"
-#pragma pop_macro("slots")
-
 Selector::Selector(QApplication *_application, QWidget *parent)
     : QMainWindow(parent)
     , application(_application)
@@ -18,20 +13,9 @@ Selector::Selector(QApplication *_application, QWidget *parent)
     // Load and display NFO
     try
     {
-        QString nfoFileName = application->applicationDirPath() + "/Team210.nfo",
-            convertedFileName = application->applicationDirPath() + "/.Team210.utf8";
-
-        // Decode CP437 using python
-        Py_SetProgramName((wchar_t*)application->arguments().first().toUtf8().data());
-        Py_Initialize();
-        PyRun_SimpleString((
-            "with open('" + nfoFileName + "', encoding='cp437') as inputFile:\n"
-            "    with open('" + convertedFileName + "','w', encoding='utf8') as outputFile:\n"
-            "        outputFile.write(inputFile.read())\n"
-        ).toUtf8().data());
-        Py_Finalize();
-
+        QString nfoFileName = application->applicationDirPath() + "/Team210.nfo";
         QFile file(nfoFileName);
+
         if(!file.open(QFile::ReadOnly)) throw;
         nfoContent = QString::fromUtf8(file.readAll());
     }
