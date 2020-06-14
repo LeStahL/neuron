9c6ed58fb8ce76681854eec0899886bfe34c9c4b
Alexander Kraus <nr4@z10.info>
2020-06-14 15:19:57 +0200

    added python interface

--- Selector.cpp
+++ Selector.cpp
@@ -3,6 +3,11 @@
 #include <QDebug>
 #include <QFile>
 
+#pragma push_macro("slots")
+#undef slots
+#include "Python.h"
+#pragma pop_macro("slots")
+
 Selector::Selector(QApplication *_application, QWidget *parent)
     : QMainWindow(parent)
     , application(_application)
@@ -13,7 +18,20 @@ Selector::Selector(QApplication *_application, QWidget *parent)
     // Load and display NFO
     try
     {
-        QFile file(application->applicationDirPath() + "/Team210.nfo");
+        QString nfoFileName = application->applicationDirPath() + "/Team210.nfo",
+            convertedFileName = application->applicationDirPath() + "/.Team210.utf8";
+
+        // Decode CP437 using python
+        Py_SetProgramName((wchar_t*)application->arguments().first().toUtf8().data());
+        Py_Initialize();
+        PyRun_SimpleString((
+            "with open('" + nfoFileName + "', encoding='cp437') as inputFile:\n"
+            "    with open('" + convertedFileName + "','w', encoding='utf8') as outputFile:\n"
+            "        outputFile.write(inputFile.read())\n"
+        ).toUtf8().data());
+        Py_Finalize();
+
+        QFile file(nfoFileName);
         if(!file.open(QFile::ReadOnly)) throw;
         nfoContent = QString::fromUtf8(file.readAll());
     }
@@ -21,6 +39,7 @@ Selector::Selector(QApplication *_application, QWidget *parent)
     {
         qDebug() << "Could not load NFO.";
     }
+
     ui.textEdit->setText(nfoContent);
 }
 
